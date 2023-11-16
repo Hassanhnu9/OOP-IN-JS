d075392f19285d2326cf64ee1faad2073faef738
unknown <Admin@LAB205-WS02.aptech-msg.edu>
2023-11-16 10:56:17 +0500

    oop file

---
+++ oop.js
@@ -0,0 +1,23 @@
+// class car{
+//     constructor(color,name,model){
+//         this.color=color;
+//         this.name=name;
+//         this.model=model;
+//     }
+// }
+
+// let car1= new car("black","bmw m5",2023)
+// console.log(car1.model)
+
+// task
+
+ class person{
+    construcotr(name,age,country){
+        this.name=name;
+        this.age=age;
+        this.contact=country;
+    }
+    details(){
+        console.log()
+    }
+ }
